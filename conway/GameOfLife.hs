9e268f51fcb08ec5b0a56098fcbd7eed8d29610f
Agustin Ramos <zentimental.software@gmail.com>
2011-11-27 04:36:01 -0600

    My pattern matching in evolveCell was naive

--- conway/GameOfLife.hs
+++ conway/GameOfLife.hs
@@ -1,11 +1,12 @@
-﻿module Conway.GameOfLife
-( newUniverse,
-  evolve,
+﻿module GameOfLife
+(  newUniverse, evolve
+  , Cell(..), Universe(..)
+  , evolveRow, evolveCell, toInt
 ) where
 
 import Data.List
 
-type Cell = Int 
+data Cell = O | X deriving (Eq, Show) -- O=Dead, X=Live
 data Universe = Universe { width :: Int, 
                            height :: Int,
                            rows :: [[Cell]]
@@ -22,24 +23,24 @@ evolveRow universe idx =
 	    next = if idx == (height universe - 1) then head cells else cells !! (idx + 1)
 	    allPrev = fst . splitAt idx $ cells
 	    allNext = tail . snd . splitAt idx $ cells
-	    newRow = map evolveCell $ allNeighborhoods4CellsInRow prev row next
+	    newRow = map evolveCell $ neighborhoods4CellsInRow prev row next
 	in Universe { width = width universe,
 	              height = height universe, 
 	              rows = allPrev ++ newRow:allNext }
 
-allNeighborhoods4CellsInRow :: [Cell] -> [Cell] -> [Cell] -> [[[Cell]]]
-allNeighborhoods4CellsInRow prevRow row nextRow =
+neighborhoods4CellsInRow :: [Cell] -> [Cell] -> [Cell] -> [[[Cell]]]
+neighborhoods4CellsInRow prevRow row nextRow =
 	let columns = zipWith3 (\x y z -> [x,y,z]) prevRow row nextRow
 	    nextNeighborhood = (\idx ->  transpose . take 3 . drop idx . cycle $ columns) 
 	    unordered = map nextNeighborhood [0..(length columns - 1)]
 	in last unordered : init unordered 
 
 evolveCell :: [[Cell]] -> Cell
-evolveCell neighborhood@(_:(_:cell:_:[]):_:[]) 
-	| cell == 1 && livingNeighbors `elem` [2,3] = 1
-	| cell == 0 && livingNeighbors == 3 = 1
-	| otherwise = 0
-	where livingNeighbors = (sum . concat $ neighborhood ) - cell
+evolveCell neighborhood@((_:_:_:[]):(_:cell:_:[]):(_:_:_:[])) 
+	| cell == X && livingNeighbors `elem` [2,3] = X
+	| cell == O && livingNeighbors == 3 = X
+	| otherwise = O
+	where livingNeighbors = (sum . map toInt . concat $ neighborhood ) - (toInt cell)
 
 newUniverse :: [[Cell]] -> Universe	
 newUniverse rows@(r1:r2:r3)
@@ -47,3 +48,7 @@ newUniverse rows@(r1:r2:r3)
 		= Universe {width = length r1, height = length rows, rows = rows }
 	| otherwise = error "Every row must have the same number of columns, with n >= 3"
 	where allRowsSameSize = and . map (\r -> length r == length r1) $ rows
+	
+toInt :: Cell -> Int
+toInt O = 0
+toInt X = 1
