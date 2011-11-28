8f0c8d224cce39cb4a6e51d32b2cb92b42d12a07
Agustin Ramos <zentimental.software@gmail.com>
2011-11-28 02:27:25 -0600

    First encounters with QuickCheck

---
+++ testing/QuickCheckBasic.hs
@@ -0,0 +1,39 @@
+﻿import Data.List
+import Test.QuickCheck
+
+-- Quicksort
+
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
+    where lhs = filter  (< x) xs
+          rhs = filter (>= x) xs
+
+-- Propeties for quicksort
+		  
+prop_ordered xs = ordered (qsort xs)
+    where ordered []       = True
+          ordered [x]      = True
+          ordered (x:y:xs) = x <= y && ordered (y:xs)
+
+prop_permutation xs = permutation xs (qsort xs)
+    where permutation xs ys = null (xs \\ ys) && null (ys \\ xs)
+
+prop_maximum xs         =
+    not (null xs) ==>
+        last (qsort xs) == maximum xs
+
+prop_append xs ys       =
+    not (null xs) ==>
+    not (null ys) ==>
+        head (qsort (xs ++ ys)) == min (minimum xs) (minimum ys)
+
+-- Experimenting with Generators for lists
+
+data Board = Board [[Cell]] deriving (Show)
+
+instance Arbitrary Board where
+	arbitrary = resize 3 arbitrary
+
+prop_notNull (Board xs) =
+	not ( null xs )
