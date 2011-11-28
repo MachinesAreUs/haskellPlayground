31bb18f90d01640a132b145ebe27c4eb8483e565
Agustin Ramos <zentimental.software@gmail.com>
2011-11-28 02:24:01 -0600

    First attempt at using QuickCheck on evolveCell

---
+++ conway/GameOfLifeTests.hs
@@ -0,0 +1,44 @@
+﻿module GameOfLifeTests
+where
+
+import GameOfLife
+import Test.QuickCheck
+
+-- Temp
+
+data Board = Board [[Cell]] deriving (Show)
+
+instance Arbitrary Board where
+	arbitrary = resize 3 arbitrary
+
+prop_notNull (Board xs) =
+	not ( null xs )
+
+-- Generators		
+
+instance Arbitrary Cell where
+	arbitrary = elements [X,O]
+	
+--instance Arbitrary [[Cell]] where
+--	arbitrary = resize 3 arbitrary
+				   
+-- Invariants for evolveCell
+
+prop_deathByOvercrowding neighborhood =
+	rightSize neighborhood           ==>
+	livingNeighbors neighborhood > 3 ==>
+		evolveCell neighborhood == X
+	where types = neighborhood::[[Cell]]
+
+-- Invariants for evolveRow
+
+
+-- Util
+
+rightSize xs =
+	length xs == 3 && all (\ys -> length ys == 3) xs
+
+--onlyValidValues xs = all (\x -> x `elem` [1,2]) . concat $ xs
+
+livingNeighbors neighborhood@(_:(_:cell:_:[]):_:[]) = 
+	( sum . map toInt . concat $ neighborhood ) - ( toInt cell )
