cfca8288497ce15e35a9af1b84afa3ea6c438bac
Agustin Ramos <agustin.ramos@knowtion.biz>
2012-08-01 11:45:19 -0500

    2nd implementation of FizzBuzz. Idea taken from http://pragprog.com/magazines/2012-08/thinking-functionally-with-haskell

---
+++ fizzbuzz/fizzbuzz2.hs
@@ -0,0 +1,5 @@
+﻿fizzbuzz x = let threes = cycle ["","","fizz"]
+                 fives  = cycle ["","","","","buzz"]
+             in take x $ zipWith (++) threes fives 
+		   
+main = mapM_ putStrLn $ fizzbuzz 100
