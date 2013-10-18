07fce32387429a3c733c8def72efc161f8c1a4e5
Agustin Ramos <agustin.ramos@knowtion.biz>
2013-10-17 19:40:05 -0500

    Solución inicial al problema de Zurg. http://web.engr.oregonstate.edu/~erwig/papers/Zurg_JFP04.pdf

---
+++ zurg/zurg.hs
@@ -0,0 +1,42 @@
+import Data.Maybe
+import Data.List
+import qualified Data.Map as Map  
+
+data Toy = Buzz | Woody | Rex | Hamm deriving (Eq, Ord, Show)
+type Movement = (Toy, Toy)
+type Solution = [Movement]
+
+times = 
+  Map.fromList [
+    (Buzz,  5 ),
+    (Woody, 10),
+    (Rex,   20),
+    (Hamm,  25)]
+
+time :: Toy -> Int
+time toy = fromJust $ Map.lookup toy times
+
+timeForward :: Movement -> Int
+timeForward m = (max (time (fst m)) (time (snd m)))
+
+timeBackward :: Movement -> Int
+timeBackward m = time (fst m) 
+
+timing :: Solution -> Int
+timing []     = 0
+timing (m:[]) = timeForward m
+timing (m:ms) = timeForward m + timeBackward m + timing ms
+        
+choices :: [Toy] -> [Movement]
+choices []   = []
+choices toys = filter (\(a,b) -> a /= b) $ toys >>= \a -> toys >>= \b -> return (a,b)
+
+appendToSolutions :: [Solution] -> Movement -> [Solution]
+appendToSolutions []   mov = [[mov]]
+appendToSolutions sols mov = map (\sol -> sol ++ [mov] ) sols
+
+solutions :: [Toy] -> [Solution] -> [Solution]
+solutions toys@(x:y:[]) sols = appendToSolutions sols (x,y)
+solutions toys          sols = concat $ map (\mov -> solutions (delete (snd mov) toys) (appendToSolutions sols mov)) $ choices toys
+
+zurg = minimumBy (\x y -> compare (snd x) (snd y)) $ map (\s -> (s, timing s)) $ solutions [Buzz, Woody, Rex, Hamm] []
